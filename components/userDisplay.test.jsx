0dd55171ddefdc4cd9309a15cfcecaa2c11af254
Md Soykot <md.soykot2910@gmail.com>
2022-09-07 17:12:56 +0600

    test add in redux

--- components/userDisplay.test.jsx
+++
@@ -1,9 +0,0 @@
-import { fireEvent, screen } from "@testing-library/react";
-// We're using our own custom render function and not RTL's render.
-import { renderWithProviders } from "../utils/test-utils";
-import UserDisplay from "./UserDisplay";
-
-test("fetches & receives a user after clicking the fetch user button", async () => {
-  renderWithProviders(<UserDisplay />);
-  fireEvent.click(screen.getByRole("button", { name: /Fetch user/i }));
-});
